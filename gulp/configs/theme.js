fafa3eff1efa10baa8400e5a5519b4ce6975c27f
Lojasyec <Taylan>
2021-03-12 10:42:50 -0300

    add config files

---
+++ gulp/configs/theme.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "Theme": {
+      "locale": "language",
+      "lang": "less",
+      "area": "frontend",
+      "vendor": "vendor",
+      "name": "name-theme",
+      "files": [
+        "css/styles-m",
+        "css/styles-l"
+      ]
+    }
+  };
